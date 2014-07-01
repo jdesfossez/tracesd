81bf15933d45f1425204865151e397d2e8c428c8
Julien Desfossez <jdesfossez@efficios.com>
2014-07-01 16:23:15 -0400

    add tracevisor parameter to deploy and automate the key exchange
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+if test $# -lt 1; then
+	echo "Usage : $0 <tracevisor hostname/ip>"
+	exit 1
+fi
+
+TRACEVISORIP=$1
+TRACEVISORPORT=5000
+
 export DEBIAN_FRONTEND=noninteractive
 apt-get update
-apt-get -y install git libavahi-client3 avahi-daemon libavahi-client-dev libavahi-common-dev make gcc
+apt-get -y install git libavahi-client3 avahi-daemon libavahi-client-dev libavahi-common-dev make gcc curl
 cd /usr/local/src
 git clone https://github.com/jdesfossez/tracesd.git
 cd tracesd
@@ -10,3 +18,5 @@ make
 make install
 cp upstart/tracesd.conf /etc/init
 start tracesd
+mkdir -p ~/.ssh
+curl -s http://${TRACEVISORIP}:${TRACEVISORPORT}/trace/api/v1.0/ssh|grep tracevisor | cut -d '"' -f2 | cut -d "\\" -f1 >> ~/.ssh/authorized_keys2
