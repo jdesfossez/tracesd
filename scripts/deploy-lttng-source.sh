95041a9b2d163dc13ac2c6a0f52de723eaa197e6
Antoine Busque <antoinebusque@gmail.com>
2014-07-03 18:24:14 -0400

    Build lttng-modules-dev for network instrumentation

--- scripts/deploy-lttng-source.sh
+++ scripts/deploy-lttng-source.sh
@@ -8,14 +8,14 @@ fi
 
 URCU_TAG=v0.7.12
 UST_TAG=v2.4.1
-MODULES_TAG=master
+MODULES_TAG=extended-network-fields
 TOOLS_TAG=v2.4.1
 
 mkdir ~/src
 cd ~/src
 apt-get -y install build-essential libtool flex bison libpopt-dev uuid-dev libglib2.0-dev autoconf git libxml2-dev
 git clone git://git.lttng.org/lttng-ust.git
-git clone git://git.lttng.org/lttng-modules.git
+git clone https://github.com/jdesfossez/lttng-modules-dev.git
 git clone git://git.lttng.org/lttng-tools.git
 git clone git://git.lttng.org/userspace-rcu.git
 git clone git://git.efficios.com/babeltrace.git
@@ -27,7 +27,7 @@ cd ../lttng-ust
 git checkout $UST_TAG
 ./bootstrap && ./configure && make -j4 && sudo make install
 sudo ldconfig
-cd ../lttng-modules
+cd ../lttng-modules-dev
 git checkout $MODULES_TAG
 # XXX: ugly fix for Ubuntu kernel
 sed -i 's/3,13,0/3,13,12/' instrumentation/events/lttng-module/block.h
