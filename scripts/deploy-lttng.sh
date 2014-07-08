d938643cab3f8bd7fb7799b6b12f90abd87c761c
Julien Desfossez <jdesfossez@efficios.com>
2014-07-08 14:02:05 -0400

    install lttng from the PPA and don't start the relay by default
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>

--- scripts/deploy-lttng.sh
+++ scripts/deploy-lttng.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 source /etc/lsb-release
-if [ "$DISTRIB_ID" != "Ubuntu" -o "$DISTRIB_RELEASE" != "14.04" ]; then
-	echo "ERROR: Only Ubuntu 14.04 is supported."
+if [ "$DISTRIB_ID" != "Ubuntu" ]; then
+	echo "ERROR: Only Ubuntu is supported."
 	exit 1
 fi
 
+apt-add-repository ppa:lttng/ppa
+
 apt-get update
 apt-get -y install lttng-modules-dkms lttng-tools babeltrace
-cp /etc/init/lttng-sessiond.conf /etc/init/lttng-relayd.conf
-sed -i 's/sessiond/relayd/g' /etc/init/lttng-relayd.conf
-start lttng-relayd
